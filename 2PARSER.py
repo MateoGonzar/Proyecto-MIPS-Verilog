0f7deb02708bf867b077ea1431b467c5bb268e90
Chel <cdeszz8@gmail.com>
2023-05-13 18:28:49 -0600

    fixed type I and R bug, missing type J inst

--- 2PARSER.py
+++ 2PARSER.py
@@ -1,11 +1,19 @@
-
-# Diccionario
+ # Diccionario
 instrucciones = {
     "add": ("100000", "00000", "", "", "", "000000"),
+    "sub": ("100010", "00000", "", "", "", "000000"),
+    "slt": ("101010", "00000", "", "", "", "000000"),
+    #Type I
+    "addi": ("001000", "", "", "", "", ""),
+    "beq": ("000100", "", "", "", "", ""),
+    "sw": ("101011", "", "", "", "", ""),
+    "lw": ("100011", "", "", "", "", ""),
+    "j": ("000010", "", "", "", "", "")
 }
 
-arch = open('./txt.txt', 'r')
-out = open('3.txt', 'w')
+
+arch = open('./in.txt', 'r')
+out = open('out.txt', 'w')
 
 # Recorrer el archivo de entrada y escribir el código binario de cada instrucción tipo R en el archivo de salida
 for linea in arch:
@@ -14,15 +22,29 @@ for linea in arch:
     codigo = ""
 
     if instruction in instrucciones:
-        codigo += instrucciones[instruction][0]
-        codigo += instrucciones[instruction][1]
-        codigo += bin(int(campos[3][1:].replace("$", "")))[2:].zfill(5)  # rd
-        codigo += bin(int(campos[2][1:].replace("$", "")))[2:].zfill(5)  # rt
-        codigo += bin(int(campos[1][1:].replace("$", "")))[2:].zfill(5)  # rs
+        if instruction == "add" or instruction == "sub" or instruction == "slt":
+            codigo += instrucciones[instruction][0]
+            codigo += instrucciones[instruction][1]
+            codigo += bin(int(campos[3][1:].replace("$", "")))[2:].zfill(5)  # rd
+            codigo += bin(int(campos[2][1:].replace("$", "")))[2:].zfill(5)  # rt
+            codigo += bin(int(campos[1][1:].replace("$", "")))[2:].zfill(5)  # rs
+        
+        if instruction == "addi" or instruction == "beq" or instruction == "lw" or instruction == "sw":
+            codigo += instrucciones[instruction][0]
+            #codigo += bin(int(campos[3][1:].replace("$", "")))[2:].zfill(5)  # rd
+            codigo += bin(int(campos[2][1:].replace("$", "")))[2:].zfill(5)  # rt
+            codigo += bin(int(campos[1][1:].replace("$", "")))[2:].zfill(5)  # rs
+            codigo += bin(int(campos[3].replace("#", "")))[2:].rjust(16, "0")  # inmediato
+        if instruction == "j":
+            # Si es una instrucción tipo J se convierte el valor inmediato a binario y se quitan los dos primeros caracteres
+            codigo += instrucciones[instruction][0]
+            codigo += bin(int(campos[1]))[2:].zfill(26)
+        
         codigo += instrucciones[instruction][2]
         codigo += instrucciones[instruction][3]
         codigo += instrucciones[instruction][4]
         codigo += instrucciones[instruction][5]
+
 
     out.write(codigo + '\n')
 
